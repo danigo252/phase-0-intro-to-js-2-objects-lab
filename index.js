b44bafd6acd8ad669518917b63f5df608d5f57bc
daniel <danielgonzalez252@gmail.com>
2023-03-12 01:18:59 -0500

    Lab Completed

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution in this file!
+const employee =
+  {
+    "name": "Daniel",
+    "streetAddress": "218 Murray Street"
+  };
+
+function updateEmployeeWithKeyAndValue(employee, key, value)
+{
+  const NewEmployee = {...employee};
+  NewEmployee[key] = value;
+
+  return NewEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value)
+{
+  employee[key] = value;
+
+  return employee;
+
+}
+
+function deleteFromEmployeeByKey(employee, key)
+{
+  const NewEmployee ={...employee};
+
+  delete NewEmployee[key];
+
+  return NewEmployee;
+
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key)
+{
+  delete employee[key];
+
+  return employee;
+
+}
